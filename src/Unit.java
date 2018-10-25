9112755b102d72c872296f342a4f40f7cf412f4f
abolfazlghanbari23 <abolfazlghanbariaaaaaaaaaaaa@gmail.com>
2018-10-25 14:51:40 +0330

    unit added

---
+++ src/Unit.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Unit {
+
+    public ArrayList<Person> persons = new ArrayList();
+    public int getScore(){
+        int score = 2;
+        for (Person person : persons){
+            score += person.getscore();
+        }
+        return score;
+    }
+    public Unit(){
+        for (int i = 0; i < 5; i++) {
+            persons.add(new Person());
+        }
+    }
+}
